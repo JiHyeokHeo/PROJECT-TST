1bb6700f95e06e37785035277a59e12c9fb055f1
HEOJIHYEOK <vndj1212@gmail.com>
2024-12-16 18:42:36 +0900

    Nav Mesh Testing /Build Height Mesh 기능

--- PROJECT-TST/Assets/PROJECT TST/Scripts/WeaponBase.cs
+++ PROJECT-TST/Assets/PROJECT TST/Scripts/WeaponBase.cs
@@ -52,9 +52,9 @@ namespace TST
 
                 Destroy(newBullet.gameObject, bulletLifeTime);
 
-                // ¿·Ω√ ≤®µŒ±‚
-                //var effect = EffectManager.Instance.SpawnEffect(EffectType.Muzzle_6);
-                //effect.transform.SetPositionAndRotation(firePoint.position, firePoint.rotation);
+                
+                var effect = EffectManager.Instance.SpawnEffect(EffectType.Muzzle_6);
+                effect.transform.SetPositionAndRotation(firePoint.position, firePoint.rotation);
 
                 return true;
             }
